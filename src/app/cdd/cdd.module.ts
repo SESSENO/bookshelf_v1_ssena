ecc431b0a45033b6f9df600c6207eb2e94ea2050
Sesseno <sfsena@gmail.com>
2022-01-10 14:04:06 -0300

    Adição de tabela utilizando os módulos matcard, mattoolbar, maticon.

--- src/app/cdd/cdd.module.ts
+++ src/app/cdd/cdd.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { CddRoutingModule } from './cdd-routing.module';
 import { ClassesComponent } from './classes/classes.component';
+
 
 
 @NgModule({
@@ -11,7 +16,11 @@ import { ClassesComponent } from './classes/classes.component';
   ],
   imports: [
     CommonModule,
-    CddRoutingModule
+    CddRoutingModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class CddModule { }
